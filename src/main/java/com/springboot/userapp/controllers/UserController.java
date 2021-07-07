4801ca97b613947708b2acf331587e5f8c89fddd
Merkanto <miroslav.i.zhelezchev@gmail.com>
2021-07-07 11:34:51 +0300

    Create User Controller. Closes #3.

---
+++ src/main/java/com/springboot/userapp/controllers/UserController.java
@@ -0,0 +1,64 @@
+package com.springboot.userapp.controllers;
+
+import com.springboot.userapp.domain.User;
+import com.springboot.userapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUser(@PathVariable int id) {
+        User user = userService.findById(id);
+
+        if (user == null) {
+            throw new RuntimeException("User id not found - " + id);
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addUser(@RequestBody User user) {
+        user.setId(0);
+
+        userService.save(user);
+
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteUser (@PathVariable int id) {
+
+        User user = userService.findById(id);
+
+        if (user == null) {
+            throw new RuntimeException("User id not found - " + id);
+        }
+
+        userService.deleteById(id);
+
+        return "Deleted user id - " + id;
+    }
+}
